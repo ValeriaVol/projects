3d606739bccc1374ba3400c113133838c7059cde
ValeriaVol <46996064+ValeriaVol@users.noreply.github.com>
2019-05-30 22:52:56 +0300

    Add files via upload

---
+++ Movie adjectives.py
@@ -0,0 +1,48 @@
+import re
+from string import punctuation
+import nltk
+from nltk.corpus import stopwords
+from pymorphy2 import MorphAnalyzer
+import pickle
+
+morph = MorphAnalyzer()
+punct = punctuation + '«»—…“”*№–'
+stops = stopwords.words('russian')
+from nltk.tokenize import word_tokenize
+stops.extend(['что', 'это', 'так', 'вот', 'быть', 'как', 'в', 'к', 'на', 'хоть', 'после','который','свой','самый','каждый','чей'])
+
+def normalize(text):
+    words = [word.strip(punct) for word in word_tokenize(text.lower())]
+    words = [morph.parse(word)[0].normal_form for word in words]
+    words = [word for word in words if word not in stops]
+    words = [word for word in words if word.endswith('й')]
+    words = list(filter(None, words))
+    return words
+
+with open(r'C:\Users\EdUKUser\PycharmProjects\Module_A\Project' + '\\' + 'ekstaz.txt', 'r',encoding="utf-8") as openfile:
+    content = openfile.read()
+    contents_normalized = normalize(content)
+    tokenized = contents_normalized
+    freqdict = {i: tokenized.count(i) for i in set(tokenized)}
+    #sorted_freq = sorted(freqdict.items(), key=lambda item: item[1],reverse=True)  # что сортируемfreqdict.items() по какому значению key=lambda х: х[1] (мы показы ваем, что мы сортируем по часточности, по тт
+    #print(sorted_freq)
+    #sorted_freq_list=[i for i in sorted_freq]
+    #print(sorted_freq_list)
+
+with open(r'C:\Users\EdUKUser\PycharmProjects\Module_A\Project' + '\\' + 'Эмоц прил.txt', 'r', encoding="utf-8") as file:
+    dict_ = file.read()
+    dict_clear = re.sub(r'\',?', '', dict_)
+    dict1 = dict_clear.lower()[1:]
+    emotional_list=dict1.split()
+    dict_emotion = dict()
+
+    for adj in freqdict:
+        if adj in emotional_list:
+            dict_emotion[adj] =freqdict[adj]
+sorted_dict_emotion = sorted(dict_emotion.items(), key=lambda item: item[1],reverse=True)
+print(sorted_dict_emotion)
+
+
+
+
+
